28742e1b5953e54488c43a16bcd953662c7bd15d
fnm-dev <f_nmachado@hotmail.com>
2022-11-29 10:46:41 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import javax.swing.plaf.SliderUI;
 import java.util.Date;
 import java.util.List;
 
@@ -35,5 +34,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, new Department(2, null));
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
     }
 }
